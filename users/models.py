0727147b0d39c251c19347fca6d6d5d656c6f65a
zaro6289-gif <cherryblossm@mail.ru>
2026-04-27 00:04:09 +0300

    import подправлен в users.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
-                                        PermissionsMixin)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.core.validators import RegexValidator
 from django.db import models
 
